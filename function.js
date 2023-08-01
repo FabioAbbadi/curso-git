e3f901a82b07f981a291f50d18c4dc3127a1f444
FabioAbbadi <FabioAbbadi@users.noreply.github.com>
2023-08-01 20:27:39 -0300

    Commit js

---
+++ function.js
@@ -0,0 +1,17 @@
+let paragraf = document.querySelector('p');
+paragraf.style.backgroundColor = 'yellow';
+
+botao.addEventListener('click', adiciona);
+
+
+function adiciona(){
+    
+    let entrada = document.getElementById('mostrarNome');
+
+    entrada.innerHTML = nome.value;
+
+    
+    console.log(entrada);
+
+
+}
